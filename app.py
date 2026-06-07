4a70923ffcff1ce773f2c16b8e8f11427af26e65
appsstudios-oficial <appsstudiosarg@gmail.com>
2026-06-07 01:34:41 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ from flask import Flask, Response, request
 
 app = Flask(__name__)
 
-URL_CIFRADA = b"aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2FwcHNzdHVkaW9zLW9maWNpYWwvbGlzdGFzMjAyNi9yZWZzL2hlYWRzL21haW4vanVuaW8yMDI2Lm0zdQ=="
+# URL Clásica de tu GitHub convertida a Base64 (Limpia y sin bloqueos)
+URL_CIFRADA = b"aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2FwcHNzdHVkaW9zLW9maWNpYWwvbGlzdGFzMjAyNi9tYWluL2p1bmlvMjAyNi5tM3U="
 CLAVE_ACCESO = "MiTele2026"
 
 @app.route('/lista.m3u')
@@ -17,15 +18,13 @@ def obtener_lista():
     # 2. Leer quién pide la lista
     user_agent = request.headers.get('User-Agent', '').lower()
     
-    # Si entran desde un navegador web de PC puro, los bloqueamos de una
+    # Bloqueo estricto solo para navegadores web puros de PC
     navegadores_web = ['chrome', 'edge', 'firefox', 'opera']
-    
-    # Si es un navegador web puro y NO viene de una app de IPTV conocida, bloqueamos
     if any(nav in user_agent for nav in navegadores_web) and not any(rep in user_agent for rep in ['vlc', 'smarters', 'iptv']):
         return "<h1>404 Not Found</h1>The server can not find the requested page.", 404
 
     try:
-        # Para cualquier otro caso (VLC PC, VLC Celu, Smart TV), entregamos la lista
+        # Desciframos la URL en formato clásico
         url_real = base64.b64decode(URL_CIFRADA).decode('utf-8')
         headers = {'User-Agent': 'Mozilla/5.0'}
         response = requests.get(url_real, headers=headers)
